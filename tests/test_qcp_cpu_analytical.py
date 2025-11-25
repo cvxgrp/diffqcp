be4b276ca3ccd50ff9025fc81194e913352dd600
healeyq3 <healeyquill@gmail.com>
2025-11-25 10:46:55 -0800

    remove assert False

--- tests/test_qcp_cpu_analytical.py
+++ tests/test_qcp_cpu_analytical.py
@@ -154,38 +154,5 @@ def test_least_squares_cpu(getkey):
 
         print("true result shape: ", jnp.shape(true_result))
         print("dx shape: ", jnp.shape(dx[m:]))
-
-        # patdb.debug()
         
         assert jnp.allclose(true_result, dx[m:], atol=1e-8)
-
-        assert False
-
-        # assert False
-        
-        # with jax.log_compiles():
-        
-        #     start = time.perf_counter()
-        #     jvp = jax.jit(qcp.jvp)
-        #     end = time.perf_counter()
-        #     print("compilation_time was: ", end - start)
-
-        #     # patdb.debug()
-
-        #     start = time.perf_counter()
-        #     dx, dy, ds = jvp(dP, dA, dq, -db)
-        #     end = time.perf_counter()
-        #     print("solve + jit time was: ", end - start)
-
-        #     start = time.perf_counter()
-        #     dx, dy, ds = jvp(dP, dA, dq, -db)
-        #     end = time.perf_counter()
-        #     print("solve time was: ", end - start)
-
-        # assert jnp.allclose(Dx_b @ db, dx[m:], atol=1e-8)
-
-        # assert False
-
-        # time here (remember to `block_until_ready`)
-
-        # for loop perturbations; make sure jvp is only compiled once.
